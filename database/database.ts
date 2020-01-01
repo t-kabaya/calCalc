d3cd27a4d25b5d7b8db5ceb48289636f05553aa5
soniczsonic <kabaya.dori@gmail.com>
2020-01-01 15:52:29 +0900

    feature: init react-native-storage. add mockDb to stub database

---
+++ database/database.ts
@@ -0,0 +1,34 @@
+import { AsyncStorage } from 'react-native'
+import Storage from 'react-native-storage'
+import { any } from 'prop-types'
+
+export const db = new Storage({ storageBackend: AsyncStorage })
+// statelessな実装にして、テスタビリティを確保している。
+// そのため、同じファイル内にあるdbを一旦exportしてから使用している。
+
+// date-fnsのparseと、formatを使用し、可逆的な、Dateの保存を実現
+
+export const save = (db: any, date: string, data: object): void => {
+  db.save({
+    key: 'calorieState' + date, // Note: Do not use underscore("_") in key!
+    data
+  })
+}
+
+export const load = async (db: any, date: string, data: object): any => {
+  const loadResult = await db.load({
+    key: 'loginState' + date, // Note: Do not use underscore("_") in key!
+    data
+  })
+
+  return loadResult
+}
+
+// テスト用のモックdatabase
+const mockDb = {
+  data: new Map(),
+  save: ({ key, data }) => {
+    mockDb.data.set(key, data)
+  },
+  load: ({ key }) => mockDb.data.get(key)
+}
